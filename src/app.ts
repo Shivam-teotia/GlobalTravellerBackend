79bd037cac6b96f4063a82b2eb036456b8e271af
Shivam-Teotia <shivamteotia200@gmail.com>
2025-03-01 17:31:48 +0530

    add localhost in cors

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,10 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: [
+            "http://localhost:3000",
+            "https://globaltravellerfrontend.onrender.com",
+        ],
         credentials: true, // Allows cookies
     })
 );
